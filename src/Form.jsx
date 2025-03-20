8022998ffd03b34a119ba28e45a9ad81d5b19f3e
mansi7470 <mansi@codequeue.in>
2025-03-20 14:10:48 +0530

    Save progress before switching branches

--- src/Form.jsx
+++ src/Form.jsx
@@ -28,7 +28,7 @@ function FormToSheet() {
 
   return (
     <div>
-     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+     <div className="flex items-center justify-center min-h-screen bg-gray-200">
   <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
     <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">React to Sheet</h1>
     <form onSubmit={handleSubmit} className="space-y-4">
